1761bbc11c0383a3bfb3fd08cee2eced8a5e6633
Luis Sanchez <luison003@gmail.com>
2022-10-17 22:35:55 -0500

    Added motor
    Created motor that still needs specs for the real one.
    
    DEV: Motor created

--- Perejil.py
+++ Perejil.py
@@ -1,17 +1,48 @@
+from tkinter import Place
 from rocketpy import Environment, SolidMotor, Rocket, Flight
+
+class LaunchPlace:
+    Latitude = 20.141352
+    Longitude = -99.148987
+    Elevation = 2200
+    name = "ElSalitre"
 
 #Setting up the simulation
 Env = Environment(
-    railLength=5.2, latitude=32.990254, longitude=-106.974998, elevation=1400
+    railLength=5.2, 
+    latitude = LaunchPlace.Latitude,
+    longitude = LaunchPlace.Longitude, 
+    elevation= LaunchPlace.Elevation
 )
 
+
+#Get weather data from the GFS forecast
 import datetime
 
 tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 
 Env.setDate((tomorrow.year, tomorrow.month, tomorrow.day, 12))  # Hour given in UTC time
 
+#Tell the GFS forecast to get the atmopheric conditions for flight.
+#Probably not all the wind speeds or atmosoheric temperatures are available.
 Env.setAtmosphericModel(type="Forecast", file="GFS")
 
+Env.info()      #To see what the weather will look like.
 
-Env.info()
+
+### CREATE THE MOTOR PEREJIL 2
+Perejil2 = SolidMotor(
+    thrustSource=250,  #".csv",        #CSV file must not contain headers/Column1=sec/Column2=T[N]
+    burnOut=3.9,
+    grainNumber=5,
+    grainSeparation=5/1000,
+    grainDensity=1815,
+    grainOuterRadius=33/1000,
+    grainInitialInnerRadius=15 / 1000,
+    grainInitialHeight=120 / 1000,
+    nozzleRadius=33 / 1000,
+    throatRadius=11 / 1000,
+    interpolationMethod="linear",
+)
+
+Perejil2.info()
